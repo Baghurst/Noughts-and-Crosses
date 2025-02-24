e8b2628c15ccd5fd850275697468b3953322198e
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-02-24 13:41:23 +0000

    Add files via upload

---
+++ Noughts and Crosess Exercise.py
@@ -0,0 +1,10 @@
+print("The game of Noughts and Crosses begins: ")
+lst = ["o","o","o","o","o","o","o","o","o","x","x","x","x","x","x","x","x","x"]
+game = "         " # 9 spaces
+pos_1 = lst[1]
+print(lst[8:15])
+player_o = int(input("Player o, enter your first o: "))
+#if player_0 == pos_1 :
+    
+    
+player_x = input(("Player x, enter your first x: "))
