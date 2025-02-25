e08fabd983cc7d0cc34d010fcf8f8a5b59fa674d
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-02-25 13:41:07 +0000

    Update Noughts and Crosess Exercise.py

--- Noughts and Crosess Exercise.py
+++ Noughts and Crosess Exercise.py
@@ -1,10 +1,437 @@
 print("The game of Noughts and Crosses begins: ")
-lst = ["o","o","o","o","o","o","o","o","o","x","x","x","x","x","x","x","x","x"]
-game = "         " # 9 spaces
-pos_1 = lst[1]
-print(lst[8:15])
+#lst = ["o","o","o","o","o","o","o","o","o","x","x","x","x","x","x","x","x","x"]
+game = ["         "] # 9 spaces
+pos_1 = game[0]
+#print(lst[8:15])
+
+print("This is the game's grid: ")
+print(" _|_|_ \n _|_|_ \n  | | ")
+r1=[" _","|","_","|","_"]
+r2=[" _","|","_","|","_"]
+r3=[" _","|","_","|","_"]
+print(r1)
+print(r2)
+print(r3)
 player_o = int(input("Player o, enter your first o: "))
-#if player_0 == pos_1 :
+if player_o == 1 :
+    r1[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 2 :
+    r1[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 3 :
+    r1[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 4 :
+    r2[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_o == 5 :
+    r2[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 6 :
+    r2[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 7 :
+    r3[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 8 :
+    r3[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 9 :
+    r3[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
     
+player_x = int(input("Player x, enter your first x: "))
+if player_x == 1 :
+    r1[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 2 :
+    r1[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 3 :
+    r1[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 4 :
+    r2[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_x == 5 :
+    r2[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 6 :
+    r2[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 7 :
+    r3[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 8 :
+    r3[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 9 :
+    r3[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+
+player_o = int(input("Player o, enter your first o: "))
+if player_o == 1 :
+    r1[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 2 :
+    r1[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 3 :
+    r1[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 4 :
+    r2[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_o == 5 :
+    r2[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 6 :
+    r2[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 7 :
+    r3[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 8 :
+    r3[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 9 :
+    r3[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
     
-player_x = input(("Player x, enter your first x: "))
+player_x = int(input("Player x, enter your first x: "))
+if player_x == 1 :
+    r1[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 2 :
+    r1[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 3 :
+    r1[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 4 :
+    r2[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_x == 5 :
+    r2[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 6 :
+    r2[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 7 :
+    r3[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 8 :
+    r3[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 9 :
+    r3[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+    
+player_o = int(input("Player o, enter your first o: "))
+if player_o == 1 :
+    r1[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 2 :
+    r1[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 3 :
+    r1[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 4 :
+    r2[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_o == 5 :
+    r2[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 6 :
+    r2[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 7 :
+    r3[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 8 :
+    r3[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 9 :
+    r3[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+    
+player_x = int(input("Player x, enter your first x: "))
+if player_x == 1 :
+    r1[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 2 :
+    r1[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 3 :
+    r1[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 4 :
+    r2[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_x == 5 :
+    r2[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 6 :
+    r2[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 7 :
+    r3[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 8 :
+    r3[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 9 :
+    r3[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+
+player_o = int(input("Player o, enter your first o: "))
+if player_o == 1 :
+    r1[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 2 :
+    r1[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 3 :
+    r1[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 4 :
+    r2[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_o == 5 :
+    r2[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 6 :
+    r2[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 7 :
+    r3[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 8 :
+    r3[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 9 :
+    r3[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+    
+player_x = int(input("Player x, enter your first x: "))
+if player_x == 1 :
+    r1[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 2 :
+    r1[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 3 :
+    r1[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 4 :
+    r2[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_x == 5 :
+    r2[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 6 :
+    r2[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 7 :
+    r3[0] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 8 :
+    r3[2] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_x == 9 :
+    r3[4] = "x"
+    print(r1)
+    print(r2)
+    print(r3)
+    
+player_o = int(input("Player o, enter your first o: "))
+if player_o == 1 :
+    r1[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 2 :
+    r1[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 3 :
+    r1[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 4 :
+    r2[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)   
+elif player_o == 5 :
+    r2[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 6 :
+    r2[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 7 :
+    r3[0] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 8 :
+    r3[2] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+elif player_o == 9 :
+    r3[4] = "o"
+    print(r1)
+    print(r2)
+    print(r3)
+    
